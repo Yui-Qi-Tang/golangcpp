944ea5c811e64b88e9e36d718b01d69b166d0036
Yuki Tang <yuki.tang@blink.com.tw>
2018-09-07 13:23:22 +0800

    just commit

--- cpp/src/myFirst.cpp
+++ cpp/src/myFirst.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-
-int main() {
+int main(int argc, char* argv[]) {
+    std::cout << argv << std::endl;
     std::cout << "Hello World!!" << std::endl;
     std::cout << "中文!!" << std::endl;
     return 0;    
