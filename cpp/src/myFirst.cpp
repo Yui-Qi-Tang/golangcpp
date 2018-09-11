4b5eee5a426716a52a789042ed4c90b00af17733
Yui Qi Tang <yqtang1222@gmail.com>
2018-09-11 16:12:36 +0800

    update CXXFLAG of makefile; add socket demo

--- cpp/src/myFirst.cpp
+++ cpp/src/myFirst.cpp
@@ -1,25 +1,6 @@
-#include <iostream>
-#include <stdexcept>
-/*
-#include <string>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-//#include <netinet/in.h> 
-
 #include <thread>
-*/
-#include <unistd.h> 
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <stdlib.h> 
-#include <netinet/in.h> 
-#include <string.h> 
-using namespace std;
-
-void checkIfInt(char*);
-void showCommand(char* args[]);
-
-#define PORT 9000
 
 int socket_listen(int port) {
     int fd_socket;
@@ -41,9 +22,6 @@ int socket_listen(int port) {
 
     // 開始 *聆聽* (listen) 連線要求
     listen(fd_socket, 10);
-    int server_addr_len = sizeof(server_addr);
-    accept(fd_socket, (struct sockaddr *)&server_addr, (socklen_t *)&server_addr_len);
-
 
     return fd_socket;
 } // socket_listen()
@@ -55,84 +33,54 @@ void server_accpet(int fd_server, int server_port) {
         exit(0);
     }
     printf("server started at %s:%d ...\n", "127.0.0.1", server_port);
-    while(1) {
-        cout << "waitting..." << endl;
-        fd_guest = accept(fd_server, (struct sockaddr *)NULL, NULL);
-        printf("accept status %d\n", fd_guest);
-    }
+    fd_guest = accept(fd_server, (struct sockaddr *)NULL, NULL);
+    printf("accept status %d\n", fd_guest);
 }
 
+int create_client_fd() {
+
+    int client_fd = -1;
+    //memset(&client, 0, sizeof(client)); // init client; windows env doesnot support this function
+    // set client socket
+    client_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(client_fd < 0) {
+        printf("can not create client fd: %d\n", client_fd);
+        return  client_fd;
+    }
+    printf("client fd is: %d\n", client_fd);
+    return client_fd;
+}
+
+bool client_connect(char *ip, int port, int client_fd) {
+    int isConnect = -1;
+    struct sockaddr_in client;
+    client.sin_family = AF_INET;
+    client.sin_addr.s_addr = inet_addr(ip);
+    client.sin_port = htons(port);
+
+    isConnect = connect(client_fd, (struct sockaddr *)&client, sizeof(client));
+    if(isConnect < 0) {
+        return false;
+    }
+    return true;
+
+}
 
 int main(int argc, char* argv[]) {
-    // int fd_server = -1;
-    // int server_port = 9999;
-    // fd_server = socket_listen(server_port);
-    // server_accpet(fd_server, server_port);
-    int server_fd, new_socket, valread; 
-    struct sockaddr_in address; 
-    int opt = 1; 
-    int addrlen = sizeof(address); 
-    char buffer[1024] = {0}; 
-    char *hello = "Hello from server"; 
-       
-    // Creating socket file descriptor 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
-    { 
-        perror("socket failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-       
-    // Forcefully attaching socket to the port 8080 
-    // if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) 
-    // { 
-    //     perror("setsockopt"); 
-    //     exit(EXIT_FAILURE); 
-    // } 
-    address.sin_family = AF_INET; 
-    address.sin_addr.s_addr = INADDR_ANY; 
-    address.sin_port = htons( PORT ); 
-    // Forcefully attaching socket to the port 8080 
-    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
-    // if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) 
-    // { 
-    //     perror("bind failed"); 
-    //     exit(EXIT_FAILURE); 
-    // } 
-    if (listen(server_fd, 3) < 0) 
-    { 
-        perror("listen"); 
-        exit(EXIT_FAILURE); 
+    int fd_server = -1;
+    int server_port = 9000;
+    int client_fd = -1;
+    bool isConnect = false;
+    char *server_ip = "127.0.0.1";
+    fd_server = socket_listen(server_port);
+    if(fd_server < 0) {
+        printf("can not create server fd, fd code: %d\n", fd_server);
     }
-    std::cout << "Hi" << endl;
-    if ((new_socket = accept(server_fd, (struct sockaddr *)NULL, NULL))<0) 
-    { 
-        perror("accept"); 
-        exit(EXIT_FAILURE); 
-    } 
-    valread = read( new_socket , buffer, 1024); 
-    printf("%s\n",buffer ); 
-    send(new_socket , hello , strlen(hello) , 0 ); 
-    printf("Hello message sent\n"); 
+    std::thread mThread( server_accpet, fd_server, server_port ); // add serever_accept to thread pool
+    client_fd = create_client_fd();
+    isConnect = client_connect(server_ip, server_port, client_fd);
+    if(isConnect) {
+        printf( "Client connects to server has been successed!! \n");
+    }
     return 0;    
-}
-
-void showCommand(char* args[]) {
-    std::cout << ":" << args << "\n"; 
-}
-
-void checkIfInt(char *str) {
-    std::string arg = str;
-    try {
-      std::size_t pos;
-      int x = std::stoi(arg, &pos);
-      if (pos < arg.size()) {
-        std::cerr << "Trailing characters after number: " << arg << '\n';
-      }
-      std::cout << x + 1 << std::endl;
-    } catch (std::invalid_argument const &ex) {
-      std::cerr << "Invalid number: " << arg << '\n';
-    } catch (std::out_of_range const &ex) {
-      std::cerr << "Number out of range: " << arg << '\n';
-    }
-
 }
